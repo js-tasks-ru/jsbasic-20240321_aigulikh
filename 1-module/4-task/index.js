5d127bc5991e41b1717fab833e7acf7acd51d125
Aigul Khatmullina <aigulikh@mail.ru>
2024-03-26 00:20:59 +0500

    spam checking

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,13 @@
 function checkSpam(str) {
-  // ваш код...
+  str = str.toLowerCase();
+  spam1 = '1xBet';
+  spam2 = 'XXX';
+  spam1 = spam1.toLowerCase();
+  spam2 = spam2.toLowerCase();
+  if (str.includes(spam1) || str.includes(spam2)) {
+      return true;
+    } else {
+  return false;
+  }
 }
+
