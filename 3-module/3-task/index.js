32d6d2580e4274d2be793ee96470b2967e4601ad
Aigul Khatmullina <aigulikh@mail.ru>
2024-04-03 23:08:08 +0500

    module 3, tasks 1-3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,35 @@
+/***
+ npm test не проходит, уже нашла в задачнике верное решение,
+ но почему нельзя так решить - я не понимаю... ***/
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('');
+  let arr2 = [];
+
+  let ind = 0; 
+    while (ind < arr.length) {
+        if (arr[ind] != "-") {
+            arr2.push(arr[ind]);
+            ind += 1;
+        } else {
+            arr2.push(arr[ind+1].toUpperCase());
+            ind += 2;
+        }
+    }
+       console.log(arr2.join("").toString());    
 }
+
+
+
+
+  /*** почему этот код не сработал? проверяла на примерах из задачника в онлайн 
+  редакторе все было ок, ну кроме пустой строки, тут это не реализовано
+  let arr = str.split('-');
+  let arr2 = [];
+  arr2.push(arr[0]);
+
+  for (let ind = 1; ind < arr.length; ind +=1) {
+    arr2.push(arr[ind][0].toUpperCase() + arr[ind].slice(1));
+  }
+
+  console.log(arr2.join(""));
+  ***/
