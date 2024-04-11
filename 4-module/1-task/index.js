99206334824646672a1bc33cbc40656c03d48740
Aigul Khatmullina <aigulikh@mail.ru>
2024-04-12 00:51:38 +0500

    1,2 tasks fixed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+
+  for (const file of friends) {
+    let li = document.createElement('li');
+    li.innerHTML = file.firstName + " " + file.lastName;
+    ul.append(li);
+  }
+
+  return ul;
 }
