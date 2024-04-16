9063485e8abd8add6d7f36ac9bca948fd2b9f4c1
Aigul Khatmullina <aigulikh@mail.ru>
2024-04-16 23:39:50 +0500

    MODULE 5 mthfck

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,27 +1,4 @@
 function namify(users) {
   return users.map(user => user.name);
 }
-/*** 
-let str = '-webkit-transition';
-let arr = str.split('-');
 
-
-
-function camelize(str) {
-    let arr = str.split('-');
-    let arr2 = [];
-    arr2.push(arr[0])
-    for (let ind = 1; ind < arr.length; ind +=1) {
-         arr2.push(arr[ind][0].toUpperCase() + arr[ind].slice(1));
-        }
-        console.log(arr2.join(""));    
-    }
-    
- camelize(str) 
- 
- 
- function filterRange(arr, a, b) {
- return arr.filter(user => (user >= a && user <= b));
-}
-
-***/
--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,10 @@
 function hideSelf() {
-  // ваш код...
+   
+  let button = document.querySelector(".hide-self-button");
+      
+  function hideButton () {
+    this.hidden = true;
+  }
+  
+  button.addEventListener('click', hideButton);
 }
