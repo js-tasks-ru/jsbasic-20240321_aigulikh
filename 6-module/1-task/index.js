5f521e2d22f6302adc36d175fcbb39a3daa0a607
Aigul Khatmullina <aigulikh@mail.ru>
2024-04-23 00:46:26 +0500

    task 6.1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,54 @@
  *      }
  *
  */
+import createElement from '../../assets/lib/create-element.js';
 export default class UserTable {
+
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.#render();
+  }
+
+  #template() {
+   return `
+   <table>
+        <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+        </thead>
+          <tbody>
+          ${this.rows.map(obj =>
+            `
+          <tr>
+          <td>${obj.name}</td>
+          <td>${obj.age}</td>
+          <td>${obj.salary}</td>
+          <td>${obj.city}</td>
+          <td><button>X</button></td>
+        </tr>
+          `).join('')}
+      </tbody>
+      </table>`
+  }
+
+  #render() {
+    this.elem = createElement(this.#template());
+
+    let buttons = this.elem.querySelectorAll("button");
+    
+    buttons.forEach( btn => {
+      btn.addEventListener('click', this.#deleteRow)
+      });
+
+    return this.elem;
+  }
+
+  #deleteRow = (event) => {
+      event.target.closest("tr").remove()
   }
 }
